2c4a38f37e7107eb6ac959df64e54b5ed64d1173
zzzmmm1874 <820374853@qq.com>
2019-01-25 21:58:00 +0800

    xiu

--- Scripts/test_001.py
+++ Scripts/test_001.py
@@ -1,10 +1,10 @@
-#import  allure
+import  allure
 class Test_001:
-    #@allure.step(title="我是test_001测试步骤名称")
+    @allure.step(title="我是test_001测试步骤名称")
     def test_001(self):
         print("---->test_001")
         assert  True
-    #@allure.step(title="我是test_002测试步骤名称")
+    @allure.step(title="我是test_002测试步骤名称")
     def test_002(self):
         print("---->test_002")
         assert  False
